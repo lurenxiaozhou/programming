a9446a22920d08f380574153f5a647300c7c9b9a
lurenxiaozhou <1063828967@qq.com>
2019-06-06 11:36:03 +0800

    app，db创建配置的抽取

---
+++ info/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from redis import  StrictRedis
+from flask_wtf import CSRFProtect
+from flask_session import Session
+
+
+
+
+
+app = Flask(__name__)
+# 1.集成配置类
+app.config.from_object(Config)
+# 2.配置SQLAlchemy
+db = SQLAlchemy(app)
+# 3.配置redis
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+# 4. 配置CSRFProtect
+CSRFProtect(app)
+# 5. 配置Session
+Session(app)
--- manage.py
+++ manage.py
@@ -1,23 +1,8 @@
-from flask  import Flask,session
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_wtf import CSRFProtect
-from flask_session import Session
+from flask  import session
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
-from config import Config
+from info import app,db
 
-app = Flask(__name__)
-# 1.集成配置类
-app.config.from_object(Config)
-# 2.配置SQLAlchemy
-db = SQLAlchemy(app)
-# 3.配置redis
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-# 4. 配置CSRFProtect
-CSRFProtect(app)
-# 5. 配置Session
-Session(app)
 # 6. 配置manager
 manager = Manager(app)
 # 7. 设置sql迁移
