a6ce09d5ea5fc7b1acec5e9d9b1382ad00505ee4
lurenxiaozhou <1063828967@qq.com>
2019-06-06 15:06:19 +0800

    使用工厂函数对配置进行进一步优化

--- info/__init__.py
+++ info/__init__.py
@@ -6,17 +6,19 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 
 
+# 创建db分成两部，使用init_app
+db = SQLAlchemy()
 
-
-
-app = Flask(__name__)
-# 1.集成配置类
-app.config.from_object(config['develop'])
-# 2.配置SQLAlchemy
-db = SQLAlchemy(app)
-# 3.配置redis
-redis_store = StrictRedis(host=config['develop'].REDIS_HOST,port=config['develop'].REDIS_PORT)
-# 4. 配置CSRFProtect
-CSRFProtect(app)
-# 5. 配置Session
-Session(app)
+def create_app(config_name):
+    app = Flask(__name__)
+    # 1.集成配置类
+    app.config.from_object(config[config_name])
+    # 2.配置SQLAlchemy
+    db.init_app(app)
+    # 3.配置redis
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    # 4. 配置CSRFProtect
+    CSRFProtect(app)
+    # 5. 配置Session
+    Session(app)
+    return app
--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 from flask  import session
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
-from info import app,db
+from info import create_app,db
 
+# 通过传入不同配置创造出不同配置下的app实例，工厂方法
+app = create_app('develop')
 # 6. 配置manager
 manager = Manager(app)
 # 7. 设置sql迁移
