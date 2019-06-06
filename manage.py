1b9a04d06589492afbbf7830db15dfade721358f
lurenxiaozhou <1063828967@qq.com>
2019-06-06 11:25:24 +0800

    配置文件单独存放

--- manage.py
+++ manage.py
@@ -5,25 +5,7 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
-class Config(object):
-    DEBUG = True
-    SECRET_KEY = 'dawangjiaowolaixunshan'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/programming'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    #redis类属性
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-    #Session配置
-    #指定存储方式
-    SESSION_TYPE = 'redis'
-    #指定存储对象redis的地址端口
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    #设置永久保存为false
-    SESSION_PERMANENT = False
-    # 设置签名加密
-    SESSION_USE_SIGNER = True
-    #设置保存时间两天
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
+from config import Config
 
 app = Flask(__name__)
 # 1.集成配置类
