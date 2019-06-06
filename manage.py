733735b0f69dbd52b4232c0fcece436cd14d2778
lurenxiaozhou <1063828967@qq.com>
2019-06-06 09:28:22 +0800

    基本配置

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 from flask  import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 class Config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/programming'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 
 
 app = Flask(__name__)
 # 1.集成配置类
 app.config.from_object(Config)
-
+# 2.配置SQLAlchemy
+db = SQLAlchemy(app)
 @app.route("/")
 def index():
     return  "hello world"
