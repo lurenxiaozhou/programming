f2a33cd07931159e8d6f54bdd3ef07b341c6148b
lurenxiaozhou <1063828967@qq.com>
2019-06-08 09:38:15 +0800

    数据库迁移

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask  import session
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
 from info import create_app,db
-
+from info.models import *
 # 通过传入不同配置创造出不同配置下的app实例，工厂方法
 app = create_app('develop')
 # 6. 配置manager
