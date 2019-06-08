f78f77a3d7c1df7d3121258366d90c8d9898eaa5
lurenxiaozhou <1063828967@qq.com>
2019-06-08 10:24:09 +0800

    图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_file, current_app, redirect
 from info.modules.index import index_blu
 
 
@@ -7,3 +7,12 @@ def index():
 
 
     return  render_template('news/index.html')
+
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # 图标加载
+    # return send_file("/static/news/favicon.ico")
+    # redirect("/static/news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
