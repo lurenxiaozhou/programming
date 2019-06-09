ed358af1b8dd6f0ec0daba33c42a054db71a5342
lurenxiaozhou <1063828967@qq.com>
2019-06-09 18:49:28 +0800

    登录右上角

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,12 +1,25 @@
-from flask import render_template, send_file, current_app, redirect
+from flask import render_template, send_file, current_app, redirect, session
 from info.modules.index import index_blu
+from info.models import User
 
 
 @index_blu.route("/")
 def index():
+    # 需求：首页右上角实现
+    # 当我们进入到首页。我们需要判断用户是否登录，将用户信息查出来，渲染给index.html
+    user_id = session.get("user_id") #获取到当前session保存的用户
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    # 如果user为空那么传一个None，如果不为空user.to_dict
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
 
-
-    return  render_template('news/index.html')
+    return  render_template('news/index.html',data =data)
 
 
 
