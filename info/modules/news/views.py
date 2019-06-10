2f7dae8aa380cc041e9282cc2be3a3b5e5dc3d1d
lurenxiaozhou <1063828967@qq.com>
2019-06-10 16:06:57 +0800

    模板抽取继承

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,9 @@
 from info.modules.news import news_blu
-from flask import render_template
+from flask import render_template,session,current_app
+
+from info.models import User
+
+
 @news_blu.route('/<int:news_id>')
 def detail(news_id):
     """
@@ -7,4 +11,15 @@ def detail(news_id):
     :param news_id:
     :return:
     """
-    return render_template("news/detail.html")
+    user_id = session.get("user_id")
+    user = None
+
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    data = {
+        "user_info":user.to_dict() if user else None
+    }
+    return render_template("news/detail.html",data = data)
