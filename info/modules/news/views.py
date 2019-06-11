8dc1c5e90b5f6fac4691471dfb094948f59aff7a
lurenxiaozhou <1063828967@qq.com>
2019-06-11 19:39:12 +0800

    收藏后端

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,8 +1,58 @@
 import constants
 from info.modules.news import news_blu
-from flask import render_template, session, current_app, g, abort
+from flask import render_template, session, current_app, g, abort, jsonify, request
 from info.utils.common import user_login
 from info.models import News
+from utils.response_code import RET
+
+
+@news_blu.route('/news_collect',methods=["POST"])
+@user_login
+def news_collect():
+    """
+    新闻的收藏与取消收藏
+    1.接收参数
+    2.效验参数
+    3.收藏新闻和取消收藏
+    4.返回响应
+    :return:
+    """
+    # 用户登录状态
+    user = g.user
+    if not user:
+        return jsonify(errno = RET.SESSIONERR,errmsg = "用户未登录")
+    # 接收参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+    # 效验参数
+    if not all([news_id,action]):
+        return jsonify(errno = RET.PARAMERR,errmsg = "参数错误")
+    if action not in ['collect', 'cancel_collect']:
+        return jsonify(errno = RET.PARAMERR,errmsg = "参数错误")
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno = RET.PARAMERR,errmsg = "数据格式不正确")
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno = RET.DBERR,errmsg = "数据库查询失败")
+    if not news:
+        return jsonify(errno = RET.NODATA,errmsg = "该条新闻不存在")
+
+    #收藏新闻和取消收藏
+    if action == "collect":
+        # 当去们去收藏当前新闻的时候要判断他是否已经收藏过了，收藏过就不在收藏
+        if news in user.collection_news:
+            user.collection_news.append(news)
+    else:
+        # 当去取消的时候判断他是否在收藏列表中，
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    return jsonify(errno = RET.OK,errmsg = "OK")
+
 
 
 @news_blu.route('/<int:news_id>')
