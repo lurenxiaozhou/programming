aa5ed120d6fc4bff90fc0cdc435c021140cba4d9
lurenxiaozhou <1063828967@qq.com>
2019-06-11 12:54:44 +0800

    新闻详情页显示：判断新闻是不是存在

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,6 @@
 import constants
 from info.modules.news import news_blu
-from flask import render_template, session, current_app, g
+from flask import render_template, session, current_app, g, abort
 from info.utils.common import user_login
 from info.models import News
 
@@ -22,6 +22,25 @@ def detail(news_id):
     except Exception as e:
         current_app.logger.error(e)
     clicks_news_li = [news.to_basic_dict() for news in clicks_news]
+
+    # 2.显示新闻的具体信息
+    if not news_id:
+        abort(404) # 判断新闻id存不存在
+        # 类型是不是整数类型
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(404)
+    # 判断新闻是不是存在
+    news = None
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        abort(404)
 
 
     data = {
