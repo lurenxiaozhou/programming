1e5724c53bb3be8e15e7f506d0c5949c5d215864
lurenxiaozhou <1063828967@qq.com>
2019-06-11 12:48:54 +0800

    详情页显示排行

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,8 @@
+import constants
 from info.modules.news import news_blu
 from flask import render_template, session, current_app, g
 from info.utils.common import user_login
-
+from info.models import News
 
 
 @news_blu.route('/<int:news_id>')
@@ -13,7 +14,18 @@ def detail(news_id):
     :return:
     """
     user = g.user
+
+    # 1.查询点击排行新闻
+    clicks_news = []
+    try:
+        clicks_news = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS).all()
+    except Exception as e:
+        current_app.logger.error(e)
+    clicks_news_li = [news.to_basic_dict() for news in clicks_news]
+
+
     data = {
-        "user_info":user.to_dict() if user else None
+        "user_info":user.to_dict() if user else None,
+        "clicks_news_li":clicks_news_li
     }
     return render_template("news/detail.html",data = data)
