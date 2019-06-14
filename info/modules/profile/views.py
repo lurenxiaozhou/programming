a534598a66a2b312c1b46dee31ea08293b1349cd
lurenxiaozhou <1063828967@qq.com>
2019-06-14 10:22:53 +0800

    个人中心-基本资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,10 +1,12 @@
-from flask import g, redirect,render_template
+from flask import g, redirect,render_template,request,jsonify,current_app
 
+from info import db
 from info.modules.profile import profile_blu
 from info.utils.common import user_login
+from utils.response_code import RET
 
 
-@profile_blu.route('/user_base_info')
+@profile_blu.route('/user_base_info',methods = ["GET","POST"])
 @user_login
 def user_base_info():
     """
@@ -12,20 +14,35 @@ def user_base_info():
     :return:
     """
     user = g.user
+    if request.method == "GET":
+        data = {
+            "user_info":user.to_dict()
+        }
 
-    data = {
-        "user_info":user.to_dict()
-    }
+        return render_template("news/user_base_info.html",data = data)
 
-    return render_template("news/user_base_info.html",data = data)
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
 
+    if not all([nick_name,signature,gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
 
+    if gender not in ['MAN','WOMAN']:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
 
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
 
-
-
-
-
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    
+    return jsonify(errno=RET.OK, errmsg="OK",data = user.to_dict())
 
 
 @profile_blu.route('/info')
