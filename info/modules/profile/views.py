e0d3d6cf5954dea38f6332492eeec83d765919d7
lurenxiaozhou <1063828967@qq.com>
2019-06-14 15:12:20 +0800

    个人中心-图片设置并上传七牛

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,12 +1,14 @@
 from flask import g, redirect,render_template,request,jsonify,current_app
 
+from info import constants
 from info import db
 from info.modules.profile import profile_blu
 from info.utils.common import user_login
+from info.libs.image_storage import storage
 from utils.response_code import RET
 
 
-@profile_blu.route('/user/user_pic_info')
+@profile_blu.route('/user/user_pic_info',methods = ["GET","POST"])
 @user_login
 def user_pic_info():
     """
@@ -14,11 +16,37 @@ def user_pic_info():
     :return:
     """
     user = g.user
-    data = {
-        "user_info":user.to_dict()
-    }
+    if request.method == "GET":
+        data = {
+            "user_info":user.to_dict()
+        }
 
-    return render_template("news/user_pic_info.html",data =data)
+        return render_template("news/user_pic_info.html",data =data)
+
+    try:
+        image_data = request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    #上传图片
+    try:
+        key = storage(image_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传头像失败")
+
+    user.avatar_url = key
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="保存数据库失败")
+
+    return jsonify(errno=RET.OK, errmsg="上传头像成功",data = constants.QINIU_DOMIN_PREFIX + key)
+
 
 
 @profile_blu.route('/user_base_info',methods = ["GET","POST"])
