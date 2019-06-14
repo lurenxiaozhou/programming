4aa6d203422a08399feb904f53ea0668bc358e62
lurenxiaozhou <1063828967@qq.com>
2019-06-14 09:51:24 +0800

    个人中心-基本资料显示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -4,6 +4,30 @@ from info.modules.profile import profile_blu
 from info.utils.common import user_login
 
 
+@profile_blu.route('/user_base_info')
+@user_login
+def user_base_info():
+    """
+    宣言基本资料界面
+    :return:
+    """
+    user = g.user
+
+    data = {
+        "user_info":user.to_dict()
+    }
+
+    return render_template("news/user_base_info.html",data = data)
+
+
+
+
+
+
+
+
+
+
 @profile_blu.route('/info')
 @user_login
 def user_info():
