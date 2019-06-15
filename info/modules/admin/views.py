97ddf4a8fafb172e2e39ceb31b10a73d0f233f99
lurenxiaozhou <1063828967@qq.com>
2019-06-15 13:07:02 +0800

    后台登录界面,完成

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,10 +1,36 @@
 from info.modules.admin import admin_blu
-from flask import render_template
+from flask import render_template, request, current_app, session
 
-@admin_blu.route("/login")
+from info.models import User
+
+
+@admin_blu.route("/login",methods = ["GET","POST"])
 def login():
     """
     后台登录页面
     :return:
     """
-    return render_template("admin/login.html")
+    if request.method == "GET":
+        return render_template("admin/login.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not all([username,password]):
+        return render_template("admin/login.html",errmsg = "请输入用户名或密码")
+
+    try:
+        user = User.quer.filter(User.mobile ==username,User.is_admin==1).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("admin/login.html",errmsg = "数据库查询错误")
+    if not user:
+        return render_template("admin/login.html",errmsg = "用户不存在")
+
+    if not user.check_passowrd(password):
+        return render_template("admin/login.html",errmsg = "密码不正确")
+
+    session["user_id"] = user.id
+    session["is_admin"] = user.is_admin
+
+    return "重定向到首页"
