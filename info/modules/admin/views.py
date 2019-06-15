afa9c36bdb94a97669775921223a1014f1659be0
lurenxiaozhou <1063828967@qq.com>
2019-06-15 20:56:16 +0800

    新闻分类管理后端逻辑

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -10,6 +10,24 @@ from info.utils.response_code import RET
 from info.libs.image_storage import storage
 
 
+@admin_blu.route('/news_category')
+def get_news_category():
+    # 获取所有的分类数据
+    categories = Category.query.all()
+    # 定义列表保存分类数据
+    categories_dicts = []
+
+    for category in categories:
+        # 获取字典
+        cate_dict = category.to_dict()
+        # 拼接内容
+        categories_dicts.append(cate_dict)
+
+    categories_dicts.pop(0)
+    # 返回内容
+    return render_template('admin/news_type.html', data={"categories": categories_dicts})
+
+
 @admin_blu.route('/news_edit_detail', methods=["GET", "POST"])
 def news_edit_detail():
     """新闻编辑详情"""
