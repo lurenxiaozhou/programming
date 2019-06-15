4acabe4f84558683c08aa0c56ec309291ee8399b
lurenxiaozhou <1063828967@qq.com>
2019-06-15 20:22:33 +0800

    新闻板式编辑后端逻辑实现

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -9,6 +9,49 @@ from info.models import User, News
 from info.utils.response_code import RET
 
 
+@admin_blu.route('/news_edit')
+def news_edit():
+    """返回新闻列表"""
+
+    page = request.args.get("p", 1)
+    keywords = request.args.get("keywords", "")
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        filters = []
+        # 如果有关键词
+        if keywords:
+            # 添加关键词的检索选项
+            filters.append(News.title.contains(keywords))
+
+        # 查询
+        paginate = News.query.filter(*filters) \
+            .order_by(News.create_time.desc()) \
+            .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_basic_dict())
+
+    context = {"total_page": total_page, "current_page": current_page, "news_list": news_dict_list}
+
+    return render_template('admin/news_edit.html', data=context)
+
+
 @admin_blu.route('/news_review_detail', methods=["GET", "POST"])
 def news_review_detail():
     """新闻审核"""
