55a0900148a30f310f015f1d367b025e27245ed8
lurenxiaozhou <1063828967@qq.com>
2019-06-15 19:23:24 +0800

    用户统计后端逻辑

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from info import user_login
 from info.modules.admin import admin_blu
 from flask import render_template, request, current_app, session, redirect, url_for, g
@@ -7,7 +9,44 @@ from info.models import User
 
 @admin_blu.route('/user_count')
 def user_count():
-    return render_template('admin/user_count.html')
+    # 1.用户总数
+    total_count = User.query.filter(User.is_admin == False).count()
+
+    t = datetime.datetime.now()
+    # 2.月新增数 距今天一个月
+    #　今天的时间对象  datetime.datetime.now()
+    # 制造时间字符串“2019-06-01”
+    month_date_str = "%d-%02d-01" % (t.year,t.month)
+    month_date = datetime.datetime.strptime(month_date_str,"%Y-%m-%d")
+    month_count = User.query.filter(User.is_admin==False,User.create_time>month_date).count()
+    # 3.日新增数
+    day_data_str = "%d-%02d-%02d" % (t.year,t.month,t.day)
+    day_date = datetime.datetime.strptime(day_data_str,"%Y-%m-%d")
+    day_count = User.query.filter(User.is_admin==False,User.create_time>day_date).count()
+    #4 用户活跃数统计
+    activate_date = []
+    activate_count = []
+    for i in range(0,31):
+        start_date = day_date-datetime.timedelta(days=i-0)
+        end_date = day_date - datetime.timedelta(days=i-1)
+        count = User.query.filter(User.is_admin==False,
+                                  User.last_login>=start_date,
+                                  User.last_login<end_date).count()
+        start_date_str = start_date.strftime("%Y-%m-%d")
+        activate_date.append(start_date_str)
+        activate_count.append(count)
+    activate_count.reverse()
+    activate_date.reverse() # 排倒序
+
+    data = {
+        "total_count":total_count,
+        "month_count":month_count,
+        "day_count":day_count,
+        "activate_count":activate_count,
+        "activate_date":activate_date
+    }
+
+    return render_template('admin/user_count.html',data=data)
 
 
 
