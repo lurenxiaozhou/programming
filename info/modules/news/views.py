f276c790aae975ac2e6e82397c291d51e59b2107
lurenxiaozhou <1063828967@qq.com>
2019-06-16 16:22:42 +0800

    关注和取消关注

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -7,6 +7,60 @@ from info.models import News, Comment, CommentLike, User
 from info.utils.response_code import RET
 
 
+@news_blu.route('/followed_user', methods=["POST"])
+@user_login
+def followed_user():
+    """
+    关注取消关注
+    :return:
+    """
+    if not g.user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    user_id = request.json.get("user_id")
+    action = request.json.get("action")
+
+    if not all([user_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ("follow", "unfollow"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 查询到关注的用户信息
+    try:
+        target_user = User.query.get(user_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据库失败")
+
+    if not target_user:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到用户数据")
+
+    # 根据不同操作做不同逻辑
+    if action == "follow":
+        if target_user.followers.filter(User.id == g.user.id).count() > 0:
+            return jsonify(errno=RET.DATAEXIST, errmsg="当前已关注")
+        target_user.followers.append(g.user)
+    else:
+        if target_user.followers.filter(User.id == g.user.id).count() > 0:
+            target_user.followers.remove(g.user)
+
+    # 保存到数据库
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据保存错误")
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
+
+
+
+
+
+
 @news_blu.route('/comment_like',methods = ["POST"])
 @user_login
 def set_comment_like():
